31de6fa9329750acddfc595fa280966562f0e766
Anderson Fonseca <andersonfonseca1999@hotmail.com>
2021-04-13 15:31:14 -0300

    add theme

---
+++ src/styles/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+  white: '#fff',
+  dark: '#333',
+  green: '#12ce37',
+  greenDark: '#11a72f',
+  red: '#f11a16',
+  blue: '#539BF5',
+
+  center: 'display: flex; justify-content: center; align-items: center',
+};
+
+export default theme;
