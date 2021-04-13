3d8274e3484dbeb4b47e450d3ff02e933ccc002b
Anderson Fonseca <andersonfonseca1999@hotmail.com>
2021-04-13 15:29:57 -0300

    add file for game rules and messages

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export const GAME_RULES = [
+  { choice: 'rock', beats: 'scissors' },
+  { choice: 'scissors', beats: 'paper' },
+  { choice: 'paper', beats: 'rock' },
+];
+
+export const MESSAGES = {
+  initial: 'escolha sua jogada!',
+  loading: 'aguarde...',
+  computerWinner: 'computador venceu!',
+  playerWinner: 'você venceu!',
+  draw: 'empatou!',
+};
