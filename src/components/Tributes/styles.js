a66023496482da92749831087f77d5cac80ad879
Anderson Fonseca <andersonfonseca1999@hotmail.com>
2021-04-14 01:00:28 -0300

    fix Tributes component style

--- src/components/Tributes/styles.js
+++ src/components/Tributes/styles.js
@@ -6,12 +6,14 @@ export const Container = styled.span`
   right: 1rem;
   font-size: 1.4rem;
   color: ${({ theme }) => theme.white};
+  padding: 1.5rem;
 
   a {
     color: ${({ theme }) => theme.green};
   }
 
-  @media (max-width: 600px) {
+  @media (max-width: 550px) {
     font-size: 1.1rem;
+    bottom: -35rem;
   }
 `;
